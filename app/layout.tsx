8d26e9a9e352ffc3114387d0111f8d971ec65a28
Hoang Anh <nth.anh020209@gmail.com>
2025-04-30 11:08:40 +0700

    css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,42 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import localFont from 'next/font/local';
 import './globals.css';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
+const imbPlexSans = localFont({
+  src: [
+    { path: '/fonts/IBMPlexSans-Regular.ttf', weight: '400', style: 'normal' },
+    { path: '/fonts/IBMPlexSans-Medium.ttf', weight: '500', style: 'normal' },
+    {
+      path: '/fonts/IBMPlexSans-SemiBold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+    { path: '/fonts/IBMPlexSans-Bold.ttf', weight: '700', style: 'normal' },
+  ],
 });
 
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+const bebasNeue = localFont({
+  src: [
+    { path: '/fonts/BebasNeue-Regular.ttf', weight: '400', style: 'normal' },
+  ],
+  variable: '--font-bebas-neue',
 });
 
 export const metadata: Metadata = {
   title: 'Thuê đồ',
-  description: 'Website này cho thuê đồ',
+  description: 'Ở đây cho thuê đồ',
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang='en'>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${imbPlexSans.className} ${bebasNeue.variable} antialiased`}
       >
         {children}
       </body>
     </html>
   );
-}
+};
+
+export default RootLayout;
